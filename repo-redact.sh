4685303d7f12309b6371f5021cb443aefa7cb6df
bob <57952350+TheRedGreek@users.noreply.github.com>
2020-10-23 14:42:13 -0400

    Firest

---
+++ repo-redact.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+set -x
+
+
+list-gen () {
+  FILE_RE='/var/tmp/file-redaction.yaml'
+  REPOS=$(ls /etc/yum.repos.d/)
+  FILE_P='- /etc/yum.repos.d/'
+  echo -e "---\nfiles:" > $FILE_RE
+  for r in $REPOS; do
+    echo -e "$FILE_P$r" >> $FILE_RE
+  done
+}
+
+insights-prep () {
+  list-gen
+  FILE_IP='/etc/insights-client/file-redaction.yaml'
+  REMOVE_CONF=('remove.conf' 'file-content-redaction.yaml')
+  if rpm -q insights-client; then
+    rm -f '/etc/insights-client/file-content-redaction.yaml /etc/insights-client/remove.conf'
+    mv $FILE_RE $FILE_IP
+    insights-client
+  else
+    yum install insights-client -y
+    insights-client --register
+  fi
+}
+
+insights-prep
