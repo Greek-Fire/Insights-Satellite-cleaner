c4888c5a2db6dde348a69ae43a2cbf5d0d36fd10
ltiches <ltiches@redhat.com>
2020-10-26 15:28:23 -0400

    added master

--- repo-redact.sh
+++ repo-redact.sh
@@ -3,10 +3,12 @@
 
 list_gen () {
   FILE_RE='/var/tmp/file-redaction.yaml'
+  FILE_CT='/var/tmp/file-content-redaction.yaml'
   REPOS=$(ls /etc/yum.repos.d/)
   FILE_P='- /etc/yum.repos.d/'
   echo -e '---\nfiles:' > $FILE_RE
-  chmod 0600 $FILE_RE
+  echo -e "---\npatterns:\n  regex:\n  - '\[.*?\]'" > $FILE_CT
+  chmod 0600 $FILE_RE $FILE_CT
   for r in $REPOS; do
     echo -e "$FILE_P$r" >> $FILE_RE 
   done
@@ -48,20 +50,6 @@ temp_disable_repo_list () {
   yum clean all
 }
 
-run_insights () {
-  INSTIGHT_TEST=$(insights-client --status)
-  if [[ $INSTIGHT_TEST == 'System is NOT registered'* ]]; then
-    echo " System is NOT registered, registering..."
-    insights-client --register
-    rm -f /etc/insights-client/file-* /etc/insights-client/remove.conf
-    mv $FILE_RE /etc/insights-client/file-redaction.yaml
-  else
-    rm -f /etc/insights-client/file-* /etc/insights-client/remove.conf
-    insights-client
-    mv $FILE_RE /etc/insights-client/file-redaction.yaml
-  fi
-}
-
 enable_repo_list () {
   subscription-manager repos --enable=*satellite-tools*
   for r in ${enabled[@]}; do
@@ -71,6 +59,20 @@ enable_repo_list () {
   echo ${enabled[@]}
 }
 
+run_insights () {
+  INSTIGHT_TEST=$(insights-client --status)
+  if [[ $INSTIGHT_TEST == 'System is NOT registered'* ]]; then
+    echo " System is NOT registered, registering..."
+    insights-client --register
+    mv $FILE_RE /etc/insights-client/file-redaction.yaml
+    mv $FILE_CT /etc/insights-client/file-content-redaction.yaml
+  else
+    rm -f /etc/insights-client/file-* /etc/insights-client/remove.conf
+    insights-client
+    mv $FILE_RE /etc/insights-client/file-redaction.yaml
+    mv $FILE_CT /etc/insights-client/file-content-redaction.yaml
+  fi
+}
 
 run_time () {
   temp_disable_repo_list
